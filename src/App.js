2d1d8eb6a8869e0418a2308850f418aa7dc329a7
fiokee <smleffiong@gmail.com>
2022-11-08 00:16:19 +0100

    working on the landpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from "react";
 
 function App() {
+const categories = [{
+  id: 1,
+  title: "Hats",
+},
+
+{
+  id: 2,
+  title: "Jackets",
+},
+
+{
+  id: 3,
+  title: "Seankers",
+},
+
+{
+  id: 4,
+  title: "Women",
+},
+
+{
+  id: 5,
+  title: "Men",
+},
+]
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="categories-container">
+      {categories.map(({title})=>(
+
+      <div className="category-container">
+        <div className="background-image"/>
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      ))}
     </div>
   );
 }
